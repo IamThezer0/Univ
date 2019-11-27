908cc47e017c7bcbab46d6325b3e49f4940dd2bf
Lacatusu David <david.lacatusu@student.upt.ro>
2019-11-27 17:14:13 +0200

    degradated to standart VS

---
+++ LearnByMySelf/LearnByMySelf/InvataSingurFmm.cpp
@@ -0,0 +1,56 @@
+/*#define _CRT_SERCURE_NO_WARNINGS
+#include <iostream>
+#include<string>
+#define max 100
+
+using namespace std;
+
+void swap(int *a, int *b) {
+	int auxiliar;
+	auxiliar = *a;
+	*a = *b;
+	*b = auxiliar;
+}
+
+void bubblesort(int arr[], int n) {
+
+	int i, j;
+	for (i = 0; i < n - 1; i++)
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(&arr[j], &arr[j + 1]);
+
+}
+void printArray(int arr[], int size){
+
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+
+}
+int main() {
+	
+	int arr[] = { 35, 12, 25, 9 , 0, 122,5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	bubblesort(arr, n);
+	cout << "Sirul ordonat este: \n";
+	printArray(arr, n);
+
+
+	return 0;
+
+
+}
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<string.h>
+
+
+int main() {
+
+
+}
--- POO/main.cpp
+++ POO/main.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 int main() {
-
+    int x;
     cout << "Vasea, Sosati";
     cout << "Atest";
     cout<<"Arafaz cu buteliiiii";
+    cout<<"sadas\n";
+    cin>>x;
+    cout<<x;
+
     return 0;
 }
 
