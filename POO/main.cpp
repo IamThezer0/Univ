3c09fd33f2828fa11a95ee911eccfa2f66843f82
Lacatusu David <david.lacatusu@student.upt.ro>
2019-12-23 23:37:09 +0200

    Done with classes

--- POO/main.cpp
+++ POO/main.cpp
@@ -4,6 +4,74 @@
 
 using namespace std;
 
+class List;
+
+class Insurance{
+    string firstName,
+           lastName;
+    int CNP,
+        period,
+        type;
+        Insurance *next;
+public:
+    Insurance( string f_name, string l_name, int cod, int per, int tip)
+    {
+        firstName = f_name;
+        lastName = l_name;
+        CNP = cod;
+        period = per;
+        type = tip; 
+        next = NULL;
+    }
+    virtual void display()
+    {
+        cout<<"------------------------------------\n";
+        cout<<"First name is : "<<firstName<<endl;
+        cout<<"Last name is : "<<lastName<<endl;
+        cout<<"The CNP of the client is: "<<CNP<<endl;
+        cout<<"The period of insurance is: "<<period<<endl;
+    }
+    friend class List;
+};
+class travelInsurance : public Insurance{
+    string destination;
+public:
+    travelInsurance(string f_name, string l_name, int cod, int per, int tip, string dest) : Insurance(f_name, l_name,cod,per,tip)
+    {
+        destination = dest;
+    }
+    void display()
+    {
+        Insurance::display();
+        cout<<"The destination of travel is: "<<destination<<endl; 
+    }
+    friend class List;
+};
+
+class carInsurance : public Insurance
+{
+    string carType;
+    string carModel;
+public:
+    carInsurance(string f_name, string l_name, int cod, int per, int tip, string car_t, string car_m) : Insurance( f_name, l_name, cod, per, tip)
+    {
+        carType = car_t;
+        carModel = car_m;
+    }
+    void display()
+    {
+        Insurance::display();
+        cout<<"The type of the car is: "<<carType <<endl;
+        cout<<"The model of the car is: "<<carModel<<endl;
+    }
+    friend class List;
+};
+// Ma duc sa imi fac un ceai si sa ma pun la serial. :)
+// Daca ai chef sa te mai uiti peste astea sa vezi daca sunt ok.
+//P.S.: Ti pwp si Craciun Fericit. <3
+
+
+
 int main(){
     
  }
